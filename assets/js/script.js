a94aa651f05b9ef671ec486b16c44eb3d7d26823
Jacob Rubino <jacobrubino@gmail.com>
2023-03-01 22:15:32 -0500

    got the first fetch to work

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,25 +11,26 @@ let daysWeath
 // }
 
 function fetchWeather(search) {
-  fetch(search)
+  return fetch(search)
     .then((response) => response.json())
-    .then(data => {return data;})
+    .then((data) => data)
     .catch((error) => console.log(error));
 }
 function getLonLat(cityName) {
   let geotoLatId = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName},3166-2:US&appid=${WAppId}`
   console.log(geotoLatId)
-  return fetchWeather(geotoLatId, (cityData) => {
-    console.log(cityData)
+  let result = fetchWeather(geotoLatId, () => {
+  
   });
+  console.log(result.PromiseResult)
 }
-getLonLat('Chicago')
+getLonLat('New York')
 
-console.log(weatherLI);
-for (let index = 0; index < weatherLI.length; index++) {
-  const element = weatherLI[index];
-  element.textContent = "";
-}
+// console.log(weatherLI);
+// for (let index = 0; index < weatherLI.length; index++) {
+//   const element = weatherLI[index];
+//   element.textContent = "";
+// }
 function searchSubmit(event) {
   event.preventDefault();
   let searchBarInput = document.querySelector("#search-bar");
@@ -37,55 +38,55 @@ function searchSubmit(event) {
 }
 
 
-function printResults(resultObj) {
-  console.log(resultObj);
+// function printResults(resultObj) {
+//   console.log(resultObj);
 
-  // set up `<div>` to hold result content
-  let WeatherWidg = document.createElement('div');
-  WeatherWidg.classList.add('card', 'bg-light', 'text-dark', 'mb-3', 'p-3');
+//   // set up `<div>` to hold result content
+//   let WeatherWidg = document.createElement('div');
+//   WeatherWidg.classList.add('card', 'bg-light', 'text-dark', 'mb-3', 'p-3');
 
-  let finalBody = document.createElement('div');
-  finalBody.classList.add('card-body');
-  WeatherWidg.append(finalBody);
+//   let finalBody = document.createElement('div');
+//   finalBody.classList.add('card-body');
+//   WeatherWidg.append(finalBody);
 
-  let titleEl = document.createElement('h3');
-  titleEl.textContent = resultObj.title;
+//   let titleEl = document.createElement('h3');
+//   titleEl.textContent = resultObj.title;
 
-  let bodyContentEl = document.createElement('p');
-  bodyContentEl.innerHTML =
-    '<strong>Date:</strong> ' + resultObj.date + '<br/>';
+//   let bodyContentEl = document.createElement('p');
+//   bodyContentEl.innerHTML =
+//     '<strong>Date:</strong> ' + resultObj.date + '<br/>';
 
-  if (resultObj.subject) {
-    bodyContentEl.innerHTML +=
-      '<strong>Subjects:</strong> ' + resultObj.subject.join(', ') + '<br/>';
-  } else {
-    bodyContentEl.innerHTML +=
-      '<strong>Subjects:</strong> No subject for this entry.';
-  }
+//   if (resultObj.subject) {
+//     bodyContentEl.innerHTML +=
+//       '<strong>Subjects:</strong> ' + resultObj.subject.join(', ') + '<br/>';
+//   } else {
+//     bodyContentEl.innerHTML +=
+//       '<strong>Subjects:</strong> No subject for this entry.';
+//   }
 
-  if (resultObj.description) {
-    bodyContentEl.innerHTML +=
-      '<strong>Description:</strong> ' + resultObj.description[0];
-  } else {
-    bodyContentEl.innerHTML +=
-      '<strong>Description:</strong>  No description for this entry.';
-  }
+//   if (resultObj.description) {
+//     bodyContentEl.innerHTML +=
+//       '<strong>Description:</strong> ' + resultObj.description[0];
+//   } else {
+//     bodyContentEl.innerHTML +=
+//       '<strong>Description:</strong>  No description for this entry.';
+//   }
 
-  let linkButtonEl = document.createElement('a');
-  linkButtonEl.textContent = 'Read More';
-  linkButtonEl.setAttribute('href', resultObj.url);
-  linkButtonEl.classList.add('btn', 'btn-dark');
+//   let linkButtonEl = document.createElement('a');
+//   linkButtonEl.textContent = 'Read More';
+//   linkButtonEl.setAttribute('href', resultObj.url);
+//   linkButtonEl.classList.add('btn', 'btn-dark');
 
-  finalBody.append(titleEl, bodyContentEl, linkButtonEl);
+//   finalBody.append(titleEl, bodyContentEl, linkButtonEl);
 
-  resultContentEl.append(WeatherWidg);
-}
-function returnWeath(){
-  for (let index = 0; index < FiveDArr.length; index += 8) {
-    daysWeath = FiveDArr[index]
-    printResults(daysWeath.ITEMHERE)
+//   resultContentEl.append(WeatherWidg);
+// }
+// function returnWeath(){
+//   for (let index = 0; index < FiveDArr.length; index += 8) {
+//     daysWeath = FiveDArr[index]
+//     printResults(daysWeath.ITEMHERE)
     
-  }
-}
+//   }
+// }
 
-searchFormEl.addEventListener("submit", returnWeath);
+// searchFormEl.addEventListener("submit", returnWeath);
