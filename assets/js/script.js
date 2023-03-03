2303484b23ee1cae35f32127423eaf32859e110a
Jacob Rubino <jacobrubino@gmail.com>
2023-03-03 12:03:25 -0500

    struggling

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,9 +2,11 @@ let weatherTURL = "";
 let weatherLI = document.getElementById("result-content").children;
 let searchFormEl = document.querySelector("#search-form");
 let WAppId = "8aa8cd47805d9d880b2338a0944a512d";
-let FiveDUrl;
 let daysWeath;
 let lonOrLat;
+let lon;
+let lat;
+let FiveDUrl = ''
 
 function fetchWeather(search) {
   return fetch(search)
@@ -18,29 +20,33 @@ function getWeath(cityName) {
   let geotoLatId = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&appid=${WAppId}`;
   return fetchWeather(geotoLatId)
     .then((geoData)=>{
-
-      let lon = geoData[0].lon;
-      let lat = geoData[0].lat;
+      lon = geoData[0].lon;
+      lat = geoData[0].lat;
       console.log(lon)
       console.log(lat)
-      let FiveDUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${WAppId}&units=imperial`
-      console.log(FiveDUrl)
-      return fetchWeather(FiveDUrl)
-        .then((weatherData) => {
-          console.log(weatherData.list[8].main.temp)
-          getWeath(weatherData.list)
-          return 'weatherdata.temp'
-      })
+        
+      // console.log(FiveDUrl)
+//       return fetchWeather(FiveDUrl)
+//         .then((weatherData) => {
+//           console.log(weatherData.list[8].main.temp)
+//           getWeath(weatherData.list)
+//           return 'weatherdata.temp'
+//       })
   })
 }
 
 
-getWeath('detroit')
-function returnWeath(weathData) {
-for (let index = 0; index < weathData.length; index += 8) {
-  const element = weatherData[index];
-  console.log(element)
-  }
+
+
+function returnWeath() { 
+  getWeath('detroit')
+  FiveDUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${WAppId}&units=imperial`
+  fetchWeather(FiveDUrl)
+  .then((weathData) => {for (let index = 0; index < weathData.length; index += 8) {
+      const element = weatherData[index];
+      console.log(element)
+    } 
+  })
 
 
   
