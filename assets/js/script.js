2dfdce3b4381260a55b2a39b847b0ae421e8daa6
Jacob Rubino <jacobrubino@gmail.com>
2023-07-03 18:05:58 -0400

    got rid of extra code

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,14 +9,6 @@ let long;
 let lat;
 let CityInput;
 let inputName;
-// let Atlanta = document.getElementById("atlanta");
-// let Denver = document.getElementById("Denver");
-// let Seattle = document.getElementById("Seattle");
-// let SanFrancisco = document.getElementById("SanFrancisco");
-// let Orlando = document.getElementById("Orlando");
-// let NewYork = document.getElementById("NewYork");
-// let Chicago = document.getElementById("Chicago");
-// let Austin = document.getElementById("Austin");
 let citybtn = document.getElementsByClassName("city-button");
 let searchButtEL = document.getElementById('searchButt');
 let searchbarEl = document.getElementById("search-bar");
@@ -25,7 +17,7 @@ let searchFormEl = document.getElementById("search-form");
 
 function capitalizeFirstLetter(string) {
   return string.charAt(0).toUpperCase() + string.slice(1);
-} //got this on stackoverflow 
+}
 
 function fetchWeather(search){
   return fetch(search)
@@ -35,7 +27,6 @@ function fetchWeather(search){
 }
 
 function getWeath(cityName) {
-  // console.log(cityName)
   let geotoLatId = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&appid=${WAppId}`;
   return fetchWeather(geotoLatId).then((geoData) => {
     console.log(geoData);
@@ -45,7 +36,6 @@ function getWeath(cityName) {
 }
 
 function returnWeath(input) {
-  // console.log(input)
   inputName = capitalizeFirstLetter(input)
   getWeath(input)
     .then((lonlat) => {
@@ -57,12 +47,9 @@ function returnWeath(input) {
     })
     .then((LonLatURL) => {
       fetchWeather(LonLatURL).then((object) => {
-        // console.log(object);
-        // console.log(object.list);
         PrintMainRes(object.list[0]);
         for (let index = 7; index < object.list.length; index += 8) {
           const element = object.list[index];
-          // console.log(element);
           printResults(element);
         }
       });
@@ -109,9 +96,6 @@ function PrintMainRes(resultObj){
 }
 
 function printResults(resultObj) {
-  // console.log(resultObj);
-
-  // set up `<div>` to hold result content
   let WeatherWidg = document.createElement("div");
   WeatherWidg.classList.add(
     "card",
@@ -162,9 +146,6 @@ function printResults(resultObj) {
   weatherLI.append(WeatherWidg);
 }
 
-// function buttInput(search){
-//   returnWeath(search)
-// }
 
 function createLocalButtons (){
   var buttCont = document.getElementById("button-container")
@@ -218,98 +199,11 @@ function storeSearch (search){
   createLocalButtons()
 }
 
-
-//   const cityStorage = {'city0' : search}
-//   const storedCities = JSON.parse(localStorage.getItem('local_search'))
-//   const cityArr = storedCities && Object.values(storedCities )
-//   console.log(cityArr, storedCities)
-//   if (storedCities != null){
-//     for (let index = 0; index < cityArr.length; index++) {
-//       const element = cityArr[index];
-//       const cityValue = `city${index}`
-//       cityStorage[cityValue] = element  
-//       console.log(element, cityValue, cityStorage[cityValue])  
-//       localStorage.setItem('local_search', JSON.stringify(cityStorage)) 
-//     }
-
-//   } else {
-//     localStorage.setItem('local_search', JSON.stringify(cityStorage)) 
-//   }
-// }
-  // console.log(localStorCity)
-
-  // let count = Object.keys.length 
-  // console.log(count)
-  // // localStorage.setItem('local_search', JSON.stringify(storedCities + search))
-  // if (storedCities != null){
-  //   storedCities = storedCities + "," + `city${count}:` + search + 
-  //   localStorage.setItem('local_search',JSON.stringify(storedCities))
-       
-  // } else{
-  //   localStorage.setItem('local_search', JSON.stringify({city0:search}))
-  // }
-// }
-
-// local storage.get the object, get object.length, add 1 to it and cityname[objlen+1] = search
-
-  // if (storedCities != null){
-  // let cities = Object.values(storedCities)
-  // for (let index = 0; index < cities.length; index++) {
-  //   const element = cities[index];
-  //   if (search == element){
-  //     console.log("search is already in sotrage")
-  //   }else{
-  //     cities.unshift(search)
-  //     if (cities.length > 8){
-  //       cities.pop()
-  //       localStorage.setItem('local_search', JSON.stringify(storedCities))
-  //       }
-  //     }     
-  //   }
-  // } else {
-  //   JSON.stringify()
-  // }
-// }
-//for item in local storage, button.append(element.key )
-
-//   console.log(storedCities)
-//   var city = JSON.stringify({inputname : search});
-//   localStorage.setItem("local_search", city)
-// }
-//   if (localArr != null){
-//     if (localStorArr.includes(search, 0) == true) {
-//     console.log(search + 'was already in the array')
-//     } else{
-//       if (localStorArr.length = 8){
-//       localStorArr.unshift(search)
-//       localStorArr.splice(9,1)
-//       localStorage.setItem('local_search', localStorArr)
-//         } else {
-//           localStorArr.unshift(search)
-//           localStorage.setItem('local_search', localStorArr)
-//         }
-//       }
-//   } else {
-//     var storeAsArr = [search , "array"]
-//     console.log(`this should store ${storeAsArr}`)
-//     localStorage.setItem('local_search', storeAsArr)
-//   }
-// }
-
-// getlocal.array
-// unshift ->
-// if var newArr array.len >>> number .pop() or .splice()
-//localStorage.store arr
-//after unshift and splice, i would create buttons for each object in newArr
-//
-
-
 function submitSearch(event){
   var searchCont = searchbarEl.value
   event.preventDefault();
   storeSearch(searchCont)
   weatherLI.replaceChildren('')
-  // console.log(searchCont)
   returnWeath(searchCont)
 }
 
@@ -317,9 +211,7 @@ searchFormEl.addEventListener(("submit"), submitSearch)
 createLocalButtons()
 for (let index = 0; index < citybtn.length; index++) {
   const element = citybtn[index];
-  // console.log(element)
   element.addEventListener("click", function(){
-      // createLocalButtons()
       cityNameButt = element.id
       console.log(cityNameButt)
       returnWeath(cityNameButt)  
